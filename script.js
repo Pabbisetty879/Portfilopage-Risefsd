063542bb60d3859fcfc74165c5cf2dad5f64d4da
Pabbisetty879 <143243359+Pabbisetty879@users.noreply.github.com>
2024-08-07 19:48:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
